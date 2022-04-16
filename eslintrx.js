4199ea3181a5cd352fe14d9ff16614080e1b4b2a
aaron-edwards <edwards.aaronmark@gmail.com>
2022-04-16 20:50:42 +1000

    Adds lint

---
+++ eslintrx.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+      es6: true,
+      browser: true,
+      node: true,
+      'jest/globals': true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb-typescript'
+    ]
+}
